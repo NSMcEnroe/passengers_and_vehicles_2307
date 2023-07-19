8e4cdfb2a16e51357a36a77df41242460572f908
Nicholas McEnroe <nsmcenro@gmail.com>
2023-07-19 12:16:53 -0400

    Add passenger count progress

--- lib/park.rb
+++ lib/park.rb
@@ -1,7 +1,26 @@
 class Park
-    attr_reader :name, :admission_price
+    attr_reader :name, :admission_price, :vehicles, :passengers
     def initialize(name, admission_price)
         @name = name
         @admission_price = admission_price
+        @vehicles = []
+        @passengers = []
     end
+
+    def passenger_count(vehicle)
+        @passengers << vehicle.passengers
+        @passengers.concat
+    end
+
+
+
+    def add_vehicle(vehicle)
+        @vehicles << vehicle
+        
+        passenger_count(vehicle)
+    end
+
+
+
+
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -10,5 +10,45 @@ RSpec.describe Park do
         expect(yosemite).to be_a Park
     end
 
-    
+    it "has readable attributes" do
+        yosemite = Park.new("Yosemite", 10)
+        expect(yosemite.name).to eq("Yosemite")
+        expect(yosemite.admission_price).to eq(10)
+        expect(yosemite.vehicles).to eq([])
+    end
+
+    it "can read vehicles inside the park" do
+        yosemite = Park.new("Yosemite", 10)
+        vehicle_1 = Vehicle.new("2001", "Honda", "Civic")
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+        jude = Passenger.new({"name" => "Jude", "age" => 20})
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+        vehicle_1.add_passenger(charlie)
+        vehicle_1.add_passenger(jude)
+        vehicle_1.add_passenger(taylor)
+
+        expect(yosemite.vehicles).to eq([])
+        
+        yosemite.add_vehicle(vehicle_1)
+        expect(yosemite.vehicles).to eq([vehicle_1])
+    end
+        
+    it "can read passengers inside the park" do
+        yosemite = Park.new("Yosemite", 10)
+        vehicle_1 = Vehicle.new("2001", "Honda", "Civic")
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+        jude = Passenger.new({"name" => "Jude", "age" => 20})
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+        vehicle_1.add_passenger(charlie)
+        vehicle_1.add_passenger(jude)
+        vehicle_1.add_passenger(taylor)
+
+        
+        yosemite.add_vehicle(vehicle_1)
+
+        expect(yosemite.passengers).to eq([charlie, jude, taylor])
+    end
+        
+
+
 end
